e53a1932355b7e229e1bf21db5d7c9fa73880c8d
theTempest67 <connectafnaan67@gmail.com>
2024-02-04 01:09:47 +0530

    another random change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const routes = require("../todolist2/routes")
+const routes = require("./routes")
 const cors = require("cors")
 
 const PORT = 3005
