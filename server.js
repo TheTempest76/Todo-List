25e049f372f4fc63ed0697eabf64997aee7ac784
theTempest67 <connectafnaan67@gmail.com>
2024-02-04 00:49:07 +0530

    changed folder structure

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const routes = require("../todolist2/routes")
+const cors = require("cors")
+
+const PORT = 3005
+app.use(express.json())
+
+app.use(cors());
+app.use("/api" ,routes )
+app.get("/" , (req, res) =>{
+    res.setHeader('Content-Type', 'application/javascript');
+    res.json({status : "worko"})
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
